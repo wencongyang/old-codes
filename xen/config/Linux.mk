86ad36da7eab64039fce7f8490e635afc01153cc
Wen Congyang <wency@cn.fujitsu.com>
2013-01-31 13:00:24 +0800

    xen: build domU kernels under xen
    Signed-off-by: Wen Congyang <wency@cn.fujitsu.com>

--- xen/config/Linux.mk
+++ xen/config/Linux.mk
@@ -1,7 +1,7 @@
 include $(XEN_ROOT)/config/StdGNU.mk
 
 # You may use wildcards, e.g. KERNELS=*2.6*
-KERNELS ?= 
+KERNELS ?= linux-2.6-xenU
 
 XKERNELS := $(foreach kernel, $(KERNELS), \
               $(patsubst buildconfigs/mk.%,%, \
