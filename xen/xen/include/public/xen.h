c02abe3eca28dc0abf8ef795b9c14fc8ac74f512
Wen Congyang <wency@cn.fujitsu.com>
2013-04-30 15:43:51 +0800

    update hypercall no

--- xen/xen/include/public/xen.h
+++ xen/xen/include/public/xen.h
@@ -93,9 +93,9 @@ DEFINE_XEN_GUEST_HANDLE(xen_pfn_t);
 #define __HYPERVISOR_domctl               36
 #define __HYPERVISOR_kexec_op             37
 #define __HYPERVISOR_tmem_op              38
-#define __HYPERVISOR_reset_vcpu_op        40
-#define __HYPERVISOR_mfncopy_op           41
-#define __HYPERVISOR_which_side_op        42
+#define __HYPERVISOR_reset_vcpu_op        42
+#define __HYPERVISOR_mfncopy_op           39
+#define __HYPERVISOR_which_side_op        40
 #define __HYPERVISOR_rdwt_data_op         43
 
 /* Architecture-specific hypercall definitions. */
