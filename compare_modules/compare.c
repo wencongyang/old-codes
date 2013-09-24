2bc58aa3bcc33e7ed35b96bc3f3b7be90bf79ee5
Wen Congyang <wency@cn.fujitsu.com>
2013-09-24 13:55:25 +0800

    update default value

--- compare_modules/compare.c
+++ compare_modules/compare.c
@@ -42,11 +42,11 @@ bool compare_tcp_data = 0;
 module_param(compare_tcp_data, bool, 0644);
 MODULE_PARM_DESC(compare_tcp_data, "compare tcp data");
 
-bool ignore_tcp_window = 0;
+bool ignore_tcp_window = 1;
 module_param(ignore_tcp_window, bool, 0644);
 MODULE_PARM_DESC(ignore_tcp_window, "ignore tcp window");
 
-bool ignore_ack_difference = 0;
+bool ignore_ack_difference = 1;
 module_param(ignore_ack_difference, bool, 0644);
 MODULE_PARM_DESC(ignore_ack_difference, "ignore ack difference");
 
